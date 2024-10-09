b3a65bde72beb3be4ade175c9e6fced9501df531
jkrell444 <joshua.krellenstein@gmail.com>
2024-10-09 09:42:06 -0400

    Update streamlit_app.py
    Add section to display fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -45,3 +44,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
