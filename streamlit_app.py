5780610c68b64a6db1ef13a7859bfd55f9bc481e
jkrell444 <joshua.krellenstein@gmail.com>
2024-10-09 09:59:29 -0400

    Update streamlit_app.py
    tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,12 +24,9 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
+	ingredients_string = ''
+	
+	for fruit_chosen in ingredients_list:
 		ingredients_string += fruit_chosen + ' '
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)
