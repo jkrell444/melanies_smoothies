7243271c086481d05aa362305dd68c201b4f7923
jkrell444 <joshua.krellenstein@gmail.com>
2024-10-09 10:03:29 -0400

    Update streamlit_app.py
    Use fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,8 @@ if ingredients_list:
 	
 	for fruit_chosen in ingredients_list:
 		ingredients_string += fruit_chosen + ' '
-		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+		st.subheader(fruit_chosen + 'Nutrition Information')
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
@@ -39,4 +40,3 @@ if time_to_insert:
 	session.sql(my_insert_stmt).collect()
 	#st.success('Your Smoothie is ordered!', icon="✅")
 	st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
